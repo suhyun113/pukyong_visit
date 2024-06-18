2a5df3a5950e2d08ffbbda44e08eb439cd7bebb3
김수현 <suhyun_113@naver.com>
2024-06-19 00:39:00 +0900

    Feat: main-image 같이 회전 구현

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function rotateGrid() {
     document.querySelector('.background-grid').classList.add('rotate-left');
+    document.querySelector('.center_elements img').classList.add('rotate-left');
     document.querySelector('.floating_mini_icons').style.display = 'none';
-    document.querySelector('.center_elements').style.display = 'none';
-   }
+    document.querySelector('.center_elements button').style.display = 'none';
+}
