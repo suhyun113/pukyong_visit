5e2d08ba286addd758ef7e8d4944d69bfba1a815
김수현 <suhyun_113@naver.com>
2024-06-23 05:45:54 +0900

    Feat: img 중복으로 인한 변수 명 변경

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function toggleMusic() {
 function rotateAndMove() {
     // 회전 애니메이션 추가
     document.querySelector('.background-grid').classList.add('rotate-left');
-    document.querySelector('.center_elements img').classList.add('rotate-left');
+    document.querySelector('.center_elements img#main-image').classList.add('rotate-left');
     document.querySelector('.floating_mini_icons').style.display = 'none';
     document.querySelector('.center_elements button').style.display = 'none';
 
